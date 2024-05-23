448ddae8d177c31d60e724b85c79917d00b9ab11
Gustavo-F-E <112873809+Gustavo-F-E@users.noreply.github.com>
2024-05-23 11:17:00 -0300

    modificaciones finales en contacto y grupo 24

--- JS/contactoJS.js
+++ JS/contactoJS.js
@@ -1,10 +1,23 @@
 const d = document;
 const formulario_login = d.getElementById("formulario_login");
 const loading = d.getElementById("loading");
+const inputFile = document.getElementById('captura_problema');
+const noFilesSelectedLabel = document.querySelector('.no-files-selected');
 
 d.addEventListener("DOMContentLoaded", (e) => {
   formulario_login.classList.remove('noDisplay');
   loading.classList.add('noDisplay');
   //***********/ 
+});
+
+
+
+
+inputFile.addEventListener('change', () => {
+  if (inputFile.files.length > 0) {
+    noFilesSelectedLabel.style.display = 'none';
+  } else {
+    noFilesSelectedLabel.style.display = 'block';
+  }
 });
 
